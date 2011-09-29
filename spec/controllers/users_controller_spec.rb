5f51ac6a6338e11965f993ef3928ce9fd8d08fa7
Steve Morss <smorss@catalinacomputing.com>
2011-09-28 23:46:52 -0400

    Completed User signup.
    Added code when user save is successful.  Added integration tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -72,6 +72,29 @@ describe UsersController do
         response.should render_template('new')
       end
     end
+    
+    describe 'success' do
+      before(:each) do
+        @attr = {:name => "New User", :email => 'user@example.com',
+          :password => "password", :password_confirmation => 'password'}
+      end
+      
+      it 'should create a user' do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it 'should redirect to the user show page' do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it 'should have a welcome message' do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
   end
 
 end
